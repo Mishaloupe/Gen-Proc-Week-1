19c1baca473d386534955ef1e5e4b5b338da8d3b
Lukas BEDEN <147034100+Mishaloupe@users.noreply.github.com>
2025-11-05 13:36:21 +0100

    ajout cellular

--- Assets/Components/ProceduralGeneration/2_CellularAutomata/CellularAutomata.cs
+++ Assets/Components/ProceduralGeneration/2_CellularAutomata/CellularAutomata.cs
@@ -9,26 +9,144 @@ using UnityEngine.UIElements;
 using VTools.Grid;
 using VTools.ScriptableObjectDatabase;
 
+public class CATiles
+{
+    public Vector2 _coords = new();
+    public bool _change;
+
+    public CATiles(Vector2 coords, bool change) {  _coords = coords; _change = change;}
+}
+
 namespace Components.ProceduralGeneration.SimpleRoomPlacement
 {
     [CreateAssetMenu(menuName = "Procedural Generation Method/Cellular Automata")]
     public class CellularAutomata : ProceduralGenerationMethod
     {
         [Header("Room Parameters")]
-        [SerializeField] private int _maxRooms = 5;
-        [SerializeField] private Vector2 _minSize = new Vector2(6, 8);
-        [SerializeField] private Vector2 _maxSize = new Vector2(12, 20);
-        [SerializeField] private List<RectInt> _allRooms = new();
+        [Range(0f, 1f)]
+        [SerializeField] private float _noiseDensity = 0.5f;
+        [Range(0, 8)]
+        [SerializeField] private int _nbVoisins = 4;
+        [SerializeField] private List<CATiles> _allCells = new();
 
         protected override async UniTask ApplyGeneration(CancellationToken cancellationToken)
         {
-            // Check for cancellation
-            cancellationToken.ThrowIfCancellationRequested();
+            CreateNoise();
 
-            // Waiting between steps to see the result.
-            await UniTask.Delay(GridGenerator.StepDelay, cancellationToken: cancellationToken);
+            for (int i = 0; i < _maxSteps; i++)
+            {
+                // Check for cancellation
+                cancellationToken.ThrowIfCancellationRequested();
+
+                ChangeCell();
+
+                // Waiting between steps to see the result.
+                await UniTask.Delay(GridGenerator.StepDelay, cancellationToken: cancellationToken);
+            }
+        }
+        private void CreateNoise()
+        {
+            for (int i = 0; i < Grid.Width; i++)
+            {
+                for (int j = 0; j < Grid.Lenght; j++)
+                {
+                    if (RandomService.Chance(_noiseDensity))
+                    {
+                        if (Grid.TryGetCellByCoordinates(i, j, out var chosenCell))
+                        {
+                            AddTileToCell(chosenCell, GRASS_TILE_NAME, false);
+                        }
+                    }
+                    else
+                    {
+                        if (Grid.TryGetCellByCoordinates(i, j, out var chosenCell))
+                        {
+                            AddTileToCell(chosenCell, WATER_TILE_NAME, false);
+                        }
+                    }
+                }
+            }
         }
 
-        
+        private void ChangeCell()
+        {
+            _allCells.Clear();
+            for (int i = 0; i < Grid.Width; i++)
+            {
+                for (int j = 0; j < Grid.Lenght; j++)
+                {
+                    checkSurronding(i, j);
+                }
+            }
+
+            foreach (CATiles tiles in _allCells)
+            {
+                if (Grid.TryGetCellByCoordinates((int)tiles._coords.x, (int)tiles._coords.y, out var chosenCell))
+                {
+                    if (tiles._change == true) { AddTileToCell(chosenCell, GRASS_TILE_NAME, true); }
+                    else { AddTileToCell(chosenCell, WATER_TILE_NAME, true); }
+                }
+            }
+        }
+
+        private void checkSurronding(int i, int j)
+        {
+            int grassTiles = 0;
+            for (int a = 0; a < 3; a++)
+            {
+                for (int b = 0; b < 3; b++)
+                {
+                    if (!(a == 1 && b == 1))
+                    {
+                        if (Grid.TryGetCellByCoordinates(i - 1 + a, j - 1 + b, out var chosenCell1))
+                        {
+                            if (chosenCell1.GridObject.Template.Name == GRASS_TILE_NAME) { grassTiles++; }
+                        }
+                    }
+                }
+            }
+                
+            if (Grid.TryGetCellByCoordinates(i, j, out var chosenCell))
+            {
+                if (chosenCell.GridObject.Template.Name == GRASS_TILE_NAME)
+                {
+                    if (grassTiles < _nbVoisins)
+                    {
+                        _allCells.Add(new CATiles(new Vector2(i, j), false));
+                    }
+                }
+                else
+                {
+                    if (grassTiles >= _nbVoisins)
+                    {
+                        _allCells.Add(new CATiles(new Vector2(i, j), true));
+                    }
+                }
+            }
+        }
     }
 }
+
+
+
+
+//    if (Grid.TryGetCellByCoordinates(i - 1 + a, j - 1, out var chosenCell1))
+//    {
+//        if (chosenCell1.GridObject.Template.Name == GRASS_TILE_NAME) { grassTiles++; }
+//    }
+//}
+//for (int a = 0; a < 3; a++)
+//{
+//    if (Grid.TryGetCellByCoordinates(i - 1 + a, j + 1, out var chosenCell2))
+//    {
+//        if (chosenCell2.GridObject.Template.Name == GRASS_TILE_NAME) { grassTiles++; }
+//    }
+//}
+//if (Grid.TryGetCellByCoordinates(i + 1, j, out var chosenCell3))
+//{
+//    if (chosenCell3.GridObject.Template.Name == GRASS_TILE_NAME) { grassTiles++; }
+//}
+//if (Grid.TryGetCellByCoordinates(i - 1, j, out var chosenCell4))
+//{
+//    if (chosenCell4.GridObject.Template.Name == GRASS_TILE_NAME) { grassTiles++; }
+//}
